14162edbd40611083ebc006584ae35d71a5b8f83
Pheasant-King <waskew122@yahoo.com>
2020-07-24 15:16:46 -0400

    Move map to inside function

--- public/javascripts/Home_Map.js
+++ public/javascripts/Home_Map.js
@@ -1,12 +1,16 @@
-var LatLng = {lat: 28.6024, lng: -81.2};
-var map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 15, //street level zoom
-  mapTypeId: 'satellite', //satellite style map
-  center: LatLng, //currently hardcoded over ucf should be able to pass variable to this
-  scaleControl: true
-});
+function initMap() {
+  var LatLng = {lat: 28.6024, lng: -81.2};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15, //street level zoom
+    mapTypeId: 'satellite', //satellite style map
+    center: LatLng, //currently hardcoded over ucf should be able to pass variable to this
+    scaleControl: true
+  });
 
-var marker = new google.maps.Marker({
-  position: LatLng,
-  map: map
-});
+  var marker = new google.maps.Marker({
+    position: LatLng,
+    map: map
+  });
+}
+
+initMap();
