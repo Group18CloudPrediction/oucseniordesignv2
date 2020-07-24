022d261fd5126aa4cf0db605d0b96bbd76e1a0c0
Gustaf Jiborn <gjiborn@gmail.com>
2020-07-24 18:40:35 -0400

    Various tweaks

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('Request for home recieved');
   res.render('index', { title: 'OUC Cloud Tracking' });
 });
 
