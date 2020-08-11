6f2fcdcb9a206cd2fd4069bd7834881af84c2bc1
Pheasant-King <waskew122@yahoo.com>
2020-08-11 13:51:52 -0400

    Sample React app with routes
    Will continue to work and move files from previous app to react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+              <Route path = "/" component={Home} exact/>
+              <Route path = "/about" component={About}/>
+              <Route path = "/contact" component={Contact}/>
+              <Route component={Error}/>
+            </Switch>
+          </div>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
