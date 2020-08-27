84b720cdda8a973e88475ca0e3d5236819e56d84
Pheasant-King <waskew122@yahoo.com>
2020-08-27 07:59:08 -0400

    begin setting up location page

--- Front_End/src/components/Home_Map.js
+++ Front_End/src/components/Home_Map.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import GoogleMapReact from "google-map-react";
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
   static defaultProps = {
@@ -7,17 +7,22 @@ class Map extends Component {
       lat: 28.6024,
       lng: -81.2
     },
-    zoom: 15
+    zoom: 15,
+    options: {
+      streetViewControl: true,
+      mapTypeControl: true,
+    }
   }
 
   render () {
     return (
-      //Always explicitly set container height. Making slight change to test
-      <div id="Map" style= {{ height: "92vh", width: "100%" }}>
+      //Always explicitly set container height.
+      <div id="Map" style= {{ height: '92.5vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyCn55lIh6mJ4GnR00jjgGeWUEii5R183xA" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          options={this.props.options}
         >
         </GoogleMapReact>
       </div>
---
+++ Front_End/src/components/LocationPage_1.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Card } from "react-bootstrap";
+import GoogleMapReact from "google-map-react";
+import JsmpegPlayer from "./LivestreamPlayer.js";
+
+class LocationPage_1 extends Component {
+  render() {
+    return(
+
+    );
+  };
+};
