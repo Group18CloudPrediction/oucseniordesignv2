26863b3d8d9063d3db17c11eec6da7b98efc5a7d
goodguytim1 <timobrien56@knights.ucf.edu>
2020-08-30 11:57:40 -0400

    Tim Update
    - Change Location to St. Cloud
    - Add Markers in approximate positions

--- Front_End/src/components/Map.js
+++ Front_End/src/components/Map.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
+import Marker from './Marker';
 
 class Map extends Component {
   static defaultProps = {
     center: {
-      lat: 28.6024,
-      lng: -81.2
+      lat: 28.2389,
+      lng: -81.2312
     },
-    zoom: 15
+    zoom: 13
   }
 
   render () {
@@ -19,6 +20,30 @@ class Map extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
+        <Marker
+            key={"top-right"}
+            text={"txt"}
+            lat={28.29}
+            lng={-81.1912}
+          />
+        <Marker
+            key={"top-left"}
+            text={"txt"}
+            lat={28.2489}
+            lng={-81.2812}
+          />
+        <Marker
+            key={"bot-right"}
+            text={"txt"}
+            lat={28.2189}
+            lng={-81.1762}
+          />
+        <Marker
+            key={"bot-left"}
+            text={"txt"}
+            lat={28.1919}
+            lng={-81.2712}
+          />
         </GoogleMapReact>
       </div>
     );
