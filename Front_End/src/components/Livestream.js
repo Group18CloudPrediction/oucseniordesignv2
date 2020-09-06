800de7f04bdde7de40edd6d90e8f597e6b99f7db
Pheasant-King <waskew122@yahoo.com>
2020-09-06 11:04:11 -0400

    edit livestream.js

--- Front_End/src/components/Livestream.js
+++ Front_End/src/components/Livestream.js
@@ -14,7 +14,7 @@ class Livestream extends Component {
       <div id="Livestream">
         <JsmpegPlayer
           wrapperClassName="video-wrapper"
-          videoUrl="ws://cloudtracking-v2.herokuapp.com/stream1"
+          videoUrl="ws://cloudtracking-v2.herokuapp.com/livestream/stream1"
           options={videoOptions}
           overlayOptions={overlayOptions}
         />
