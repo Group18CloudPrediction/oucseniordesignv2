49500a4ceba80b9ee8839bc29659187f9e86c27d
goodguytim1 <timobrien56@knights.ucf.edu>
2020-09-11 12:18:21 -0400

    handleClick function

--- Front_End/src/components/Home_Map.js
+++ Front_End/src/components/Home_Map.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from './Marker';
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 class Map extends Component {
   static defaultProps = {
     center: {
@@ -15,6 +17,9 @@ class Map extends Component {
     }
   }
 
+  handleClick(key) {
+    console.log(key);
+  }
   render () {
     return (
       //Always explicitly set container height.
@@ -30,24 +35,28 @@ class Map extends Component {
             text={"txt"}
             lat={28.29172}
             lng={-81.19373}
+            onClick={() => this.handleClick("sub-28")}
           />
         <Marker
             key={"sub-27"}
             text={"txt"}
             lat={28.24917}
             lng={-81.28942}
+            onClick={() => this.handleClick("sub-27")}
           />
         <Marker
             key={"sub-29"}
             text={"txt"}
             lat={28.22465}
             lng={-81.17819}
+            onClick={() => this.handleClick("sub-29")}
           />
         <Marker
             key={"sub-33"}
             text={"txt"}
             lat={28.18127}
             lng={-81.27366}
+            onClick={() => this.handleClick("sub-33")}
           />
         </GoogleMapReact>
       </div>
