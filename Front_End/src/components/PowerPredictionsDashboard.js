cc156c3ed7515639de3c710a0c219c42bcf44888
Pheasant-King <waskew122@yahoo.com>
2020-09-18 10:50:58 -0400

    Change time from 14 to 17 in powerdashboard

--- Front_End/src/components/PowerPredictionsDashboard.js
+++ Front_End/src/components/PowerPredictionsDashboard.js
@@ -3,15 +3,15 @@ import Upcoming15MinutesLineGraph from "./apiCallers/Upcoming15MinutesLineGraph"
 
 class PowerPredictionsDashboard extends Component {
     render() {
-        
+
         // const date = new Date();
         // <Upcoming15MinutesLineGraph stationID="TEST_MANUAL_ENTRY" year={date.year} month={date.month} day={date.day} hour=[date.hour] minute={date.minute-1}/>
-        
+
         // <Upcoming15MinutesLineGraph stationID="TEST_MANUAL_ENTRY" year={2020} month={9} day={14} hour={13} minute={0}/>
-        return ( 
+        return (
             <div id="PowerPredictionsDashboard">
-                <h1> Power Predictions </h1> 
-                <Upcoming15MinutesLineGraph stationID="TEST_MANUAL_ENTRY" year={2020} month={9} day={14} hour={13} minute={0}/>
+                <h1> Power Predictions </h1>
+                <Upcoming15MinutesLineGraph stationID="TEST_MANUAL_ENTRY" year={2020} month={9} day={14} hour={17} minute={0}/>
             </div>
         );
     }
