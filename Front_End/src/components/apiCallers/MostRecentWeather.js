cb59c0f4d8f795ee42b3c8dac4ee02ae704f6fb9
Clay DiGiorgio <cldigiorgio@knights.ucf.edu>
2020-09-30 17:22:56 -0400

    added most recent weather component

---
+++ Front_End/src/components/apiCallers/MostRecentWeather.js
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+
+// I had some great help making this component from these two links
+// https://www.robinwieruch.de/react-fetching-data
+// https://dev.to/abdulbasit313/an-easy-way-to-create-a-customize-dynamic-table-in-react-js-3igg
+
+import "../../stylesheets/dataTables.css";
+import DisplayWeatherData from "../miniComponents/DisplayWeatherData.js";
+import DisplayWeatherDataFriendly from "../miniComponents/DisplayWeatherDataFriendly.js";
+
+class MostRecentWeather extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            apiResponse: "", 
+            isLoading:true,
+            hasError: false,
+            error: null
+        };
+        
+        if (!this.props.stationID)
+            this.setState({error: {message: "This component is missing a required prop: stationID"}});
+    }
+    
+    callAPI() {
+        this.setState({isLoading: true});
+        
+        // this component works whether a station id is passed or not
+        const baseURL = require("./_apiRootAddress");
+        
+        var postReqParams = {
+            stationID: this.props.stationID,
+            startDate: null,
+            startTime: null,
+            endDate: null,
+            endTime: null,
+            onlyMostRecent: 1
+        }
+        
+        var postReqURL = baseURL + "/weatherData/" + this.props.stationID;
+            
+        fetch(postReqURL, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(postReqParams)
+            })
+            .then (response => response.json()                                     )
+            .then (res      => this.setState({apiResponse: res, isLoading: false}) )
+            .catch(err      => this.setState({hasError:true, error:err})           );
+        
+    }
+    
+    componentDidMount() {
+        this.callAPI();
+    }
+    
+    render() {    
+        if (this.state.hasError) {
+            return <div>Error: <p>{this.state.error.message}</p></div>;
+        }
+        
+        if (this.state.isLoading) {
+            return <p>Loading Weather Data...</p>;
+        }
+        
+        if (!this.state.apiResponse.data) {
+            return <p>Recieved bad response</p>;
+        }
+        
+        console.log(this.state.apiResponse.data);
+        
+        if (this.props.displayFullData)
+        {
+            return ( 
+                <DisplayWeatherData apiResponseData={this.state.apiResponse.data}/>
+            );
+        }
+        else
+        {
+            return ( 
+                <DisplayWeatherDataFriendly apiResponseData={this.state.apiResponse.data}/>
+            );
+        }
+    }
+    
+}
+
+export default MostRecentWeather;
---
+++ Front_End/src/components/miniComponents/DisplayWeatherDataFriendly.js
@@ -0,0 +1,81 @@
+import React, {Component} from "react";
+
+import "../../stylesheets/dataTables.css";
+
+class DisplayWeatherDataFriendly extends Component {
+    render() {    
+        return ( 
+            <div id="RetrievedWeatherData">
+                <table className="dataTable" id="weatherDataTable">
+                    <thead className="dataTableHeader">
+                        <tr className="dataTableHeaderRow">
+                            <th className="dataTableHeaderCell">author</th>
+                            <th className="dataTableHeaderCell">system_num</th>
+                            <th className="dataTableHeaderCell">slrFD_W</th>
+                            <th className="dataTableHeaderCell">rain_mm</th>
+                            <th className="dataTableHeaderCell">strikes</th>
+                            <th className="dataTableHeaderCell">dist_km</th>
+                            <th className="dataTableHeaderCell">ws_ms</th>
+                            <th className="dataTableHeaderCell">windDir</th>
+                            <th className="dataTableHeaderCell">maxWS_ms</th>
+                            <th className="dataTableHeaderCell">airT_C</th>
+                            <th className="dataTableHeaderCell">vp_mmHg</th>
+                            <th className="dataTableHeaderCell">bp_mmHg</th>
+                            <th className="dataTableHeaderCell">rh</th>
+                            <th className="dataTableHeaderCell">rht_c</th>
+                            <th className="dataTableHeaderCell">tiltNS_deg</th>
+                            <th className="dataTableHeaderCell">tiltWE_deg</th>
+                            <th className="dataTableHeaderCell">date</th>
+                            <th className="dataTableHeaderCell">date_mins_only</th>
+                            <th className="dataTableHeaderCell">time_only</th>
+                            <th className="dataTableHeaderCell">_id</th>
+                        </tr>
+                    </thead>
+                    <tbody className="dataTableBody">
+                        {this.renderTable(this.props.apiResponseData)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    
+    renderTable(data) {
+        const round = (number, decimalPlaces) => {
+            if (isNaN(number)) return "NaN";
+            
+            const factorOfTen = Math.pow(10, decimalPlaces)
+            var retval = (Math.round(number * factorOfTen) / factorOfTen)
+            
+            return retval+"";
+        }
+        
+        return data.map((dataPoint, index) => {
+            return(
+                <tr key={dataPoint._id} className="dataTableRow">
+                    <td className="dataTableCell">{dataPoint.author}</td>
+                    <td className="dataTableCell">{dataPoint.system_num}</td>
+                    <td className="dataTableCell">{dataPoint.slrFD_W}</td>
+                    <td className="dataTableCell">{dataPoint.rain_mm}</td>
+                    <td className="dataTableCell">{dataPoint.strikes}</td>
+                    <td className="dataTableCell">{dataPoint.dist_km}</td>
+                    <td className="dataTableCell">{round(dataPoint.ws_ms, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.windDir, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.maxWS_ms, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.airT_C, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.vp_mmHg, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.bp_mmHg, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.rh, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.rht_c, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.tiltNS_deg, 3)}</td>
+                    <td className="dataTableCell">{round(dataPoint.tiltWE_deg, 3)}</td>
+                    <td className="dataTableCell">{dataPoint.date}</td>
+                    <td className="dataTableCell">{dataPoint.date_mins_only}</td>
+                    <td className="dataTableCell">{dataPoint.time_only}</td>
+                    <td className="dataTableCell">{dataPoint._id}</td>
+                </tr>
+            );
+        })
+    }
+}
+
+export default DisplayWeatherDataFriendly;
